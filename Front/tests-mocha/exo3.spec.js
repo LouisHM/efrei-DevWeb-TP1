4088388a1cd3482093aa38bc35c215b26e051921
rlrr <riyad.larbaoui@kpconsulting.fr>
2025-02-03 16:25:42 +0100

    neuille exo3

--- Front/tests-mocha/exo3.spec.js
+++ Front/tests-mocha/exo3.spec.js
@@ -6,7 +6,10 @@ import { map } from '../src/exo3.js'
 describe('ex. 2', function () {
     describe('map(array, fn)', function () {
         })
-        it('should apply the function for all values', function () {
+        it('should work with empty array', function(){
+            expect(map([]) ,item => item * 2).to.deep.eq([]);
+        })
+        it('should apply function for all values', function () {
             expect(map([1, 2, 3, 4, 5, 9, 10], item => item * 2)).to.deep.eq([2, 4,6,8,10,18,20])
         })
 })
